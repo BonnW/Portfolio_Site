55f1fe39c77fd307a0130bb4c0e9437c4b306f29
bonnw <pwonghansa@gmail.com>
2019-02-15 00:19:26 -0600

    created skeleton components

---
+++ portfolio/src/Components/InfoColumn/Sidebar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>portfolio site</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
