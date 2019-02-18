5175b18fc9b19eaab1f1ffeea5dbae09343fc422
bonnw <pwonghansa@gmail.com>
2019-02-17 22:43:25 -0600

    Started Layout Component and linked to app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import Home from "./Components/Home/Home";
-
 import "./App.css";
+
+// Component Imports
+import Layout from "./Components/Layout/Layout";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Layout />
       </div>
     );
   }
---
+++ portfolio/src/Components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Bonn's Portfolio Site</h1>
+          <h2>Work in Progress :)</h2>
+        </div>
+      </div>
+    );
+  }
+}
