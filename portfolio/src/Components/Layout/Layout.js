688bd78a354dc83f57ffe23598592572d7ea22cd
bonnw <pwonghansa@gmail.com>
2019-02-20 02:41:32 -0600

    added some styles

--- portfolio/src/Components/Layout/Layout.js
+++ portfolio/src/Components/Layout/Layout.js
@@ -6,14 +6,47 @@ import {
   Typography,
   Hidden,
   Drawer,
-  Divider
+  Divider,
+  withStyles
 } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Menu from "@material-ui/icons/Menu";
 
 const drawerWidth = 240;
 
-export default class extends Component {
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3
+  }
+});
+
+class Layout extends Component {
   state = {
     mobileOpen: false
   };
@@ -27,22 +60,22 @@ export default class extends Component {
 
     const drawer = (
       <div>
-        <div className="Drawer__Toolbar" />
+        <div className={classes.toolbar} />
         hello
         <Divider />
       </div>
     );
 
     return (
-      <div className="Drawer__Root">
+      <div className={classes.root}>
         <CssBaseline />
-        <AppBar position="fixed" className="Drawer__appbar">
+        <AppBar position="fixed" className={classes.appBar}>
           <Toolbar>
             <IconButton
               color="inherit"
               aria-label="Open drawer"
               onClick={this.handleDrawerToggle}
-              className="Drawer__MenuButton"
+              className={classes.menuButton}
             >
               <Menu />
             </IconButton>
@@ -51,7 +84,7 @@ export default class extends Component {
             </Typography>
           </Toolbar>
         </AppBar>
-        <nav className="Drawer__drawer">
+        <nav className={classes.drawer}>
           {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
           <Hidden smUp implementation="css">
             <Drawer
@@ -60,8 +93,7 @@ export default class extends Component {
               open={this.state.mobileOpen}
               onClose={this.handleDrawerToggle}
               classes={{
-                // paper: classes.drawerPaper
-                paper: "Drawer__drawerpaper"
+                paper: classes.drawerPaper
               }}
             >
               {drawer}
@@ -70,8 +102,7 @@ export default class extends Component {
           <Hidden xsDown implementation="css">
             <Drawer
               classes={{
-                // paper: "classes.drawerPaper"
-                paper: "Drawer__drawerPaper"
+                paper: classes.drawerPaper
               }}
               variant="permanent"
               open
@@ -80,11 +111,13 @@ export default class extends Component {
             </Drawer>
           </Hidden>
         </nav>
-        <main className="Drawer__content">
-          <div className="Drawer__toolbar" />
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
           {children}
         </main>
       </div>
     );
   }
 }
+
+export default withStyles(styles)(Layout);
