99aaae626c26264b2184cdd85a3ecb63ec656529
bonnw <pwonghansa@gmail.com>
2019-02-19 23:48:01 -0600

    containerized project for development

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 // Component Imports
+import Container from "./Components/Container/Container";
 import Layout from "./Components/Layout/Layout";
 import Wallpaper from "./Components/Wallpaper/Wallpaper";
 
@@ -10,8 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Layout />
-        <Wallpaper />
+        <Container />
       </div>
     );
   }
---
+++ portfolio/src/Components/Container/Container.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Layout from "../Layout/Layout";
+import Wallpaper from "../Wallpaper/Wallpaper";
+
+export default class Container extends Component {
+  render() {
+    return (
+      <div>
+        <Layout />
+        <Wallpaper />
+      </div>
+    );
+  }
+}
