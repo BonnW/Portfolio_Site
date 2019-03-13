ba33b20cd2acf331cb577b5e238e94b0d2e717da
bonnw <pwonghansa@gmail.com>
2019-03-13 07:35:12 -0500

    mvp header finished. twitter icon links to nothing currently

--- portfolio/src/Components/Header/Header.js
+++ portfolio/src/Components/Header/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
 // import profile pic from src/assets
 import pic from "../../assets/SampleProfilePic.jpg";
 
@@ -9,9 +11,24 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-        <img className="Profile__Picture" src={pic} alt="profile_pic" />
+        <img
+          className="Header__Picture"
+          src={pic}
+          alt="profile_pic"
+          onClick={console.log("clicked picture")}
+        />
         <h1>Hi I'm Bonn :)</h1>
-        <h2> -->Social Media Icons </h2>
+        <div className="Header__Icons">
+          <a target="_blank" href="https://github.com/BonnW">
+            <FaGithub size="2.5em" color="black" />
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/bonn-wonghansa/">
+            <FaLinkedin size="2.5em" color="black" />
+          </a>
+          <a target="_blank" href="">
+            <FaTwitter size="2.5em" color="black" />
+          </a>
+        </div>
       </div>
     );
   }
