aab2560437f710137f2926da49e609fce89d091e
bonnw <pwonghansa@gmail.com>
2019-03-22 16:31:37 -0500

    edited header title spacing

--- portfolio/src/Components/Header/Header.js
+++ portfolio/src/Components/Header/Header.js
@@ -3,6 +3,7 @@ import { withStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 // import profile pic from src/assets
 import pic from "../../assets/SampleProfilePic.jpg";
@@ -38,15 +39,24 @@ import pic from "../../assets/SampleProfilePic.jpg";
 // }
 
 const styles = {
-  avatarBox: {
-    width: "100%"
+  root: {
+    marginTop: 15
+  },
+
+  titleBox: {
+    marginTop: 15,
+    marginBottom: 15
+  },
+
+  titleText: {
+    marginTop: 15
   },
 
   avatar: {
     margin: 10
   },
   bigAvatar: {
-    width: "80%",
+    width: "70%",
     height: "auto",
     margin: "auto"
   }
@@ -57,24 +67,47 @@ function Header(props) {
   return (
     <div className={classes.root}>
       <Grid container justify="center" alignItems="center">
-        <Grid item xs={12}>
+        <Grid item xs={12} classes={classes.avatarBox}>
           <Avatar alt="Bonn W." src={pic} className={classes.bigAvatar} />
         </Grid>
-        <Grid item xs={12}>
-          <h1>Hi I'm Bonn :)</h1>
-          <h2>Full-Stack Engineer</h2>
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          xs={12}
+          className={classes.titleBox}
+        >
+          <Typography
+            component="h1"
+            variant="headline"
+            classes={classes.titleText}
+          >
+            Bonn Wonghansa
+          </Typography>
+          <Typography variant="title" gutterBottom>
+            Full-Stack Developer
+          </Typography>
         </Grid>
-        <div className="Header__Icons">
-          <a target="_blank" href="https://github.com/BonnW">
-            <FaGithub size="2.5em" color="black" />
-          </a>
-          <a target="_blank" href="https://www.linkedin.com/in/bonn-wonghansa/">
-            <FaLinkedin size="2.5em" color="black" />
-          </a>
-          <a target="_blank" href="">
-            <FaTwitter size="2.5em" color="black" />
-          </a>
-        </div>
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={3}>
+            <a target="_blank" href="https://github.com/BonnW">
+              <FaGithub size="2.5em" color="black" />
+            </a>
+          </Grid>
+          <Grid item xs={3}>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/bonn-wonghansa/"
+            >
+              <FaLinkedin size="2.5em" color="black" />
+            </a>
+          </Grid>
+          <Grid item xs={3}>
+            <a target="_blank" href="">
+              <FaTwitter size="2.5em" color="black" />
+            </a>
+          </Grid>
+        </Grid>
       </Grid>
     </div>
   );
