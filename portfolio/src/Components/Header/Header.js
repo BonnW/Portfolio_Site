0d4a1a39d61163a2943b97604cbfef32ec4955bd
bonnw <pwonghansa@gmail.com>
2022-07-04 02:19:43 -0500

    laptop save

--- portfolio/src/Components/Header/Header.js
+++ portfolio/src/Components/Header/Header.js
@@ -8,35 +8,6 @@ import Typography from "@material-ui/core/Typography";
 // import profile pic from src/assets
 import pic from "../../assets/SampleProfilePic.jpg";
 
-// import "./Header.css";
-// import Wallpaper from "../Wallpaper/Wallpaper";
-
-// export default class Header extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <img
-//           className="Header__Picture"
-//           src={pic}
-//           alt="profile_pic"
-//           onClick={console.log("clicked picture")}
-//         />
-//         <h1>Hi I'm Bonn :)</h1>
-//         <div className="Header__Icons">
-//           <a target="_blank" href="https://github.com/BonnW">
-//             <FaGithub size="2.5em" color="black" />
-//           </a>
-//           <a target="_blank" href="https://www.linkedin.com/in/bonn-wonghansa/">
-//             <FaLinkedin size="2.5em" color="black" />
-//           </a>
-//           <a target="_blank" href="">
-//             <FaTwitter size="2.5em" color="black" />
-//           </a>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 const styles = {
   root: {
